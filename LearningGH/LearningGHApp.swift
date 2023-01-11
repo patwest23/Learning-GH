17d8567cd8f72f3db59c79edda6d85ecc662f792
Patrick <patrickwesterkamp@gmail.com>
2023-01-10 19:33:19 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Patrick Westerkamp on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
